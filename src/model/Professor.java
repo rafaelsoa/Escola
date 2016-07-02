9feac1709ce0dfd8928df3ae379f3c69abff2aa0
a965866 <a965866@02LAB108PC07.rn.senac.br>
2016-07-01 21:17:14 -0300

    Primeiro Commit

---
+++ src/model/Professor.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author a965866
+ */
+public class Professor {
+    
+    private int matricula;
+    private String nome;
+    private int idade;
+    private String disciplina;
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" + "matricula=" + matricula + ", nome=" + nome + ", idade=" + idade + ", disciplina=" + disciplina + '}';
+    }
+    
+    
+}
